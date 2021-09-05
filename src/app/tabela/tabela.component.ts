c7644a450117fd65215c217727f52192180cb5c4
Hubert Muller <hubert@sportport.eu>
2021-09-05 14:15:36 +0200

    komponent 2

--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -1,18 +1,28 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tabela',
   templateUrl: './tabela.component.html',
   styleUrls: ['./tabela.component.scss']
 })
-export class TabelaComponent implements OnInit {
+export class TabelaComponent implements OnInit, OnDestroy {
 
-  @Input()
+  @Input('tytulTabeli')
   public tytul: string;
 
-  constructor() { }
+  public podtytul: string;
+
+  constructor() {
+    console.log('tabela - constructor');
+    this.podtytul = Math.ceil(Math.random() * 100).toString();
+  }
 
   ngOnInit(): void {
+    console.log('tabela - ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('tabela - ngOnDestroy');
   }
 
 }
