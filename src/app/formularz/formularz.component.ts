0a93fc3f4696f7d1bd8cba4e1e1a3bb701b73553
Hubert Muller <hubert@sportport.eu>
2021-09-11 14:50:49 +0200

    formularz2

--- src/app/formularz/formularz.component.ts
+++ src/app/formularz/formularz.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-formularz',
@@ -9,13 +9,59 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class FormularzComponent implements OnInit {
 
   public forma = new FormGroup({
-    imie: new FormControl('', {}),
-    nazwisko: new FormControl('', {}),
+    imie: new FormControl('', {validators: [
+      Validators.required,
+      Validators.minLength(5),
+        Validators.pattern(/foo/)
+      ], updateOn: 'blur' }),
+    nazwisko: new FormControl('', {validators: [NaszValidator.wymaganywiek(20, 40)]}),
+    haslo: new FormControl('', {}),
+    plec: new FormControl(null, {}),
+    zyczenia: new FormGroup( {
+      x: new FormControl(null, {}),
+      y: new FormControl(null, {})
+    }),
+    szczepionka: new FormControl(null, {}),
+    uwagi: new FormControl(null, {})
   });
 
   constructor() { }
 
   ngOnInit(): void {
+    this.forma.controls.imie.valueChanges.subscribe(
+      (value) => {
+        if (value === 'Hubert') {
+          console.log('jest!');
+          this.forma.controls.nazwisko.setValue('Muller');
+        }
+      }
+
+    );
+
+    this.forma.valueChanges.subscribe(
+      (value) => {
+        if (value.imie === 'Stefan' && value.nazwisko === 'Nowak') {
+          this.forma.controls.imie.setValue('');
+        }
+      }
+    );
   }
 
+  zmien(): void {
+    this.forma.controls.imie.setValue('Stefan');
+    console.log(this.forma.controls);
+  }
+}
+
+class NaszValidator {
+  static wymaganywiek(dolna: number, gorna: number) {
+    return (control: FormControl) => {
+      const wartosc = control.value;
+      if (dolna <= wartosc && gorna >= wartosc) {
+        return null;
+      } else {
+        return { poprawnyprzedzial : 'wartosc ' + wartosc + ' nie miesci sie w przedziale'};
+      }
+    };
+  }
 }
